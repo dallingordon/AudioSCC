4f790ba6b138a0d9cfb8716958d860e22b1b341d
dallingordon <dallingordon@gmail.com>
2024-09-18 15:10:07 -0400

    more to device

--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def main():
 
         for i, batch in enumerate(tqdm(dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
             wav_data, t_step, target, file_idx, seq_inputs = batch
-            wav_data, target, seq_inputs = wav_data.to(device), target.to(device), seq_inputs.to(device)
+            wav_data, t_step, target, file_idx, seq_inputs = wav_data.to(device), t_step.to(device), target.to(device), file_idx.to(device), seq_inputs.to(device)
 
             # Forward pass
             bce_output, mse_output = model(seq_inputs, file_idx, t_step)
